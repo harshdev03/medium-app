b7d93d24e6b627a624d5506b87b6381874260927
Harsh <harshchalwadi027@gmail.com>
2025-08-16 22:15:15 +0530

    updated the ui

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,13 +1,12 @@
-
 import Quote from '../components/Quote'
 import Auth from '../components/Auth'
 
 const Signup = () => {
   return (
-    <div className='grid grid-cols-2'>
+    <div className='grid grid-cols-1 lg:grid-cols-2'>
       <Auth type='signup'/>
       <div className='hidden lg:block'>
-      <Quote/>
+        <Quote/>
       </div>
     </div>
   )
