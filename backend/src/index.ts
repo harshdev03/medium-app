d8ca17f059230a428955901adff6e1b33b07fdf7
Harsh <harshchalwadi027@gmail.com>
2025-08-16 22:47:30 +0530

    added subdomain

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,35 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import user from './routes/user'
 import blog from './routes/blog'
 import middleware from './middleware'
-import { cors } from 'hono/cors'
 
 const app = new Hono()
 
-
-app.use('/*', cors({
-  origin: (origin) => {
-    const allowedOrigins = [
-      'http://localhost:5173',
-      'https://medium.harrsh.xyz',
-      'https://harrsh.xyz',
-      'https://medium-app-pearl.vercel.app'
-    ]
-    return allowedOrigins.includes(origin ?? '') ? origin : ''
-  },
-  credentials: true,
-  allowHeaders: ['Authorization', 'Content-Type'],
-}))
+app.use(
+  '/*',
+  cors({
+    origin: (origin) => {
+      const allowedOrigins = [
+        'http://localhost:5173',
+        'https://medium.harrsh.xyz',
+        'https://harrsh.xyz',
+        'https://medium-app-pearl.vercel.app',
+      ]
+      return allowedOrigins.includes(origin ?? '') ? origin : '' 
+    },
+    credentials: true,
+    allowHeaders: ['Authorization', 'Content-Type'],
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  })
+)
 
 
 app.route('/api/v1/user', user)
 app.route('/api/v1/blog', blog)
-app.route('/api/v1/' , middleware )
-
-
-app.get("/", (c) => {
-    return c.text("Post here ..")
+app.route('/api/v1', middleware) 
+app.get('/', (c) => {
+  return c.text('Post here ..')
 })
 
 export default app
